3b34bf32632cb0ba160d2fe46f5c0bf3c12ab4e1
Jailton Silva <jss1@ifal.edu.br>
2024-05-26 23:48:34 -0300

    add User and Post models

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from db.connection import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
+
+    def __repr__(self):
+        return f'f<User {self.username}>'
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50), nullable=False)
+    body = Column(String(500), nullable=False)
