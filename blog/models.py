58e2f94d12b9877ba180d929b03568f60724c33c
Jailton Silva <jss1@ifal.edu.br>
2024-05-28 00:24:02 -0300

    change: add field name in user model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from db.connection import Base
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
     username = Column(String(20), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String(50), nullable=False)
@@ -19,3 +20,6 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(50), nullable=False)
     body = Column(String(500), nullable=False)
+
+    def __repr__(self):
+        return f'f<Post {self.title}>'
