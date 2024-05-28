a682ae10289b4426f272cdc402153b5a9269a865
Jailton Silva <jss1@ifal.edu.br>
2024-05-28 00:23:07 -0300

    add users schema

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel
 
 
+class UserSchema(BaseModel):
+    username: str
+    email: str
+    password: str
+
+    class Config:
+        from_attributes = True
+
+
 class PostSchema(BaseModel):
     title: str
     body: str
