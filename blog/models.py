48ca2c002696be3c7fa5151e7729c10fa0731ddf
Jailton Silva <jss1@ifal.edu.br>
2024-05-28 12:05:19 -0300

    refact: moving user model to authentication package

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,6 @@ from sqlalchemy import Column, Integer, String, Boolean
 from db.connection import Base
 
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    username = Column(String(20), unique=True, nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
-    password = Column(String(50), nullable=False)
-
-    def __repr__(self):
-        return f'f<User {self.username}>'
-
-
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
