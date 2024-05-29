7d8a4bcec4100db9422e2a832e669955a7f0a59c
Jailton Silva <jss1@ifal.edu.br>
2024-05-29 16:36:45 -0300

    add authentication sensible data to settings class

--- settings.py
+++ settings.py
@@ -8,3 +8,6 @@ env.read_env()
 
 class Settings(BaseSettings):
     DATABASE_URL: str = env.str("DATABASE_URL")
+    SECRET_KEY: str = env.str("SECRET_KEY")
+    ALGORITHM: str = env.str("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES")
