84d2960ede26107afbc8334d7cb74e910f992acb
Jailton Silva <jss1@ifal.edu.br>
2024-05-29 16:32:21 -0300

    add function to get access token

---
+++ authentication/token.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timezone, timedelta
+from typing import Union
+
+from jose import jwt, JWTError
+
+from settings import Settings
+
+settings = Settings()
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        claims=to_encode, key=settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
