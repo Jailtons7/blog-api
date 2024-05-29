ba382d029becf24ee5a39284508819232a32074e
Jailton Silva <jss1@ifal.edu.br>
2024-05-29 16:35:31 -0300

    refact: refactoring main file app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-from environs import Env
-from fastapi import FastAPI
+from fastapi import FastAPI, responses
 
+from db.connection import engine, Base
+from authentication.routers import auth_router
 from blog.routers import blog_router
-from blog.models import Base
-from db.connection import engine
-
-env = Env()
-env.read_env()
 
 app = FastAPI()
 Base.metadata.create_all(engine)
 
 
 @app.get("/")
-async def root():
-    return {"msg": "A simple Blog API"}
+async def redirect_to_documentation():
+    return responses.RedirectResponse(url="/docs")
 
-
+app.include_router(auth_router)
 app.include_router(blog_router)
