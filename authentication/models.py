9ef1f22464093da624979b716660e103cf8a7a84
Jailton Silva <jss1@ifal.edu.br>
2024-05-30 00:37:42 -0300

    add user model

---
+++ authentication/models.py
@@ -0,0 +1,28 @@
+from passlib.context import CryptContext
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+
+from db.connection import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    username = Column(String(20), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    posts = relationship("Post", back_populates="creator")
+
+    def __repr__(self):
+        return f'f<User {self.username}>'
+
+    def set_password(self, password):
+        pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+        hashed_pwd = pwd_context.hash(password)
+        self.password = hashed_pwd
+
+    def check_password(self, password: str):
+        pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+        return pwd_context.verify(password, self.password)
