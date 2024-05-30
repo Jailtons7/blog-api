a257c30f5f2e0a245683af357daaf4221cb51a31
Jailton Silva <jss1@ifal.edu.br>
2024-05-30 00:40:54 -0300

    add the owner of the post in the Posts model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from db.connection import Base
 
@@ -8,6 +9,8 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(50), nullable=False)
     body = Column(String(500), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    creator = relationship("User", back_populates="posts")
 
     def __repr__(self):
         return f'f<Post {self.title}>'
