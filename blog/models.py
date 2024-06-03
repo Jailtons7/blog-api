2e0b857a3670902b00fd2ab4423bc758d9b74675
Jailton Silva <jss1@ifal.edu.br>
2024-06-02 23:50:10 -0300

    refact: change max length of fields username, email and password

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
 from db.connection import Base
@@ -7,8 +7,8 @@ from db.connection import Base
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    title = Column(String(50), nullable=False)
-    body = Column(String(500), nullable=False)
+    title = Column(String(150), nullable=False)
+    body = Column(Text(), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     creator = relationship("User", back_populates="posts")
 
