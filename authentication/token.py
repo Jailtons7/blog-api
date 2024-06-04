14b137e73600a94d40db4cfee16b70073d8aae2e
Jailton Silva <jss1@ifal.edu.br>
2024-06-03 22:54:14 -0300

    add functions to create and get authentication token

--- authentication/token.py
+++ authentication/token.py
@@ -2,6 +2,8 @@ from datetime import datetime, timezone, timedelta
 from typing import Union
 
 from jose import jwt, JWTError
+from fastapi import HTTPException, status
+from authentication.schemas import TokenData
 
 from settings import Settings
 
@@ -19,3 +21,19 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
         claims=to_encode, key=settings.SECRET_KEY, algorithm=settings.ALGORITHM
     )
     return encoded_jwt
+
+
+def verify_access_token(token: str) -> TokenData:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        return TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
