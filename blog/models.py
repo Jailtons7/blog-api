b83bd415b63067dca6ed44907e64ba7f987652e2
Jailton Silva <jss1@ifal.edu.br>
2024-06-10 20:25:09 -0300

    add comments model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
@@ -9,8 +11,26 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(150), nullable=False)
     body = Column(Text(), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     creator = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
 
     def __repr__(self):
         return f'f<Post {self.title}>'
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    body = Column(Text(), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable=False)
+    parent_id = Column(Integer, ForeignKey('comments.id'), nullable=True)
+
+    creator = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
+    responses = relationship("Comment", back_populates="parent", cascade="all, delete-orphan")
+    parent = relationship("Comment", back_populates="responses", remote_side=[cast("ColumnClause", id)])
+
+    def __repr__(self):
+        return f'f<Comment {self.id}>'
